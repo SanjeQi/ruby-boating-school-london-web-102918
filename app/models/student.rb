c524524d1808a9a90d58df769669c6fee2230d6e
github username <github email address>
2018-11-02 14:38:30 +0000

    add student class, all, initialize

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,23 @@
+
+require "pry"
 class Student
 
+attr_accessor :first_name
+
+
+@@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
+
+binding.pry
+puts eof
